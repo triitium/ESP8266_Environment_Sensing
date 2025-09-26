eb8752cb6f6aa65f7c7ce0c7eeece3fedaf2900a
Stefan <76401862+triitium@users.noreply.github.com>
2025-09-26 19:24:09 +0200

    Delete include directory

--- include/bme688.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-bool sensor_init();
-bool sensor_read(float &temperature, float &humidity, float &pressure, float &gas_resistance);
--- include/http_client.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-void http_post(float temperature, float humidity, float pressure, float gas_resistance);
