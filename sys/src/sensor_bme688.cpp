4b77131a07d8de3e671088b245f45fbedad6412e
Stefan <76401862+triitium@users.noreply.github.com>
2025-09-26 17:33:24 +0200

    Update sensor_bme688.cpp

--- sys/src/sensor_bme688.cpp
+++ sys/src/sensor_bme688.cpp
@@ -1,7 +1,7 @@
 #include "sensor_bme688.h"
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME680.h>  // BME688 kompatibel
+#include <Adafruit_BME680.h>
 
 #define BME_ADDR 0x76
 
@@ -13,11 +13,10 @@ bool sensor_init() {
         Serial.println("Konnte BME688 Sensor nicht finden!");
         return false;
     }
-    // Optional: Einstellungen für BME688
     bme.setTemperatureOversampling(BME680_OS_8X);
     bme.setHumidityOversampling(BME680_OS_2X);
     bme.setPressureOversampling(BME680_OS_4X);
-    bme.setGasHeater(320, 150); // 320°C für 150ms
+    bme.setGasHeater(320, 150);
     return true;
 }
 
